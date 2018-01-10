6a7d06f6fe65dd3b38e0c3ae7b2ca7e8e63faae1
iumehara <iumehara@gmail.com>
2018-01-10 15:21:11 +0900

    Adds new data to seed file

--- src/main/resources/db/seed.sql
+++ src/main/resources/db/seed.sql
@@ -1,8 +1,18 @@
-INSERT INTO restaurants (name, category_ids) VALUES ('Pintokona', '{1}');
+INSERT INTO restaurants (name, category_ids)
+VALUES
+  ('Pintokona', '{1}'),
+  ('Pizzakaya', '{2, 3}'),
+  ('Dhiya', '{4, 3}') ,
+  ('Green Asia', '{5, 3}'),
+  ('Manpukutei', '{6}')
+;
 
 INSERT INTO categories (name)
 VALUES
 ('Sushi'),
 ('Pizza'),
-('Pasta'),
-('Spicy');
+('Spicy'),
+('Curry'),
+('Southeast Asia'),
+('Meat')
+;
