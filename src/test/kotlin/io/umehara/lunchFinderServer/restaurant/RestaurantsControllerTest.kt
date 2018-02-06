23d4ebacd02d5367f18c7419ff4cb6a7ea0c2c60
iumehara <iumehara10@gmail.com>
2018-02-06 15:25:08 +0900

    removes SpringRunner from controller tests

--- src/test/kotlin/io/umehara/lunchFinderServer/restaurant/RestaurantsControllerTest.kt
+++ src/test/kotlin/io/umehara/lunchFinderServer/restaurant/RestaurantsControllerTest.kt
@@ -4,10 +4,8 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.IsEqual.equalTo
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
@@ -15,7 +13,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup
 import java.math.BigDecimal
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class RestaurantsControllerTest {
     private lateinit var repo: RestaurantRepoSpy
