f7a2cfb7934ac8a8768efd2a430c278853032c1f
iumehara <iumehara10@gmail.com>
2018-06-02 23:11:59 +0900

    adds unique constraint to restaurant and category name fields

--- src/main/resources/db/migrations.sql
+++ src/main/resources/db/migrations.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS restaurants;
 CREATE TABLE restaurants (
     id SERIAL UNIQUE,
-    name varchar(100),
-    name_jp varchar(100),
+    name varchar(100) UNIQUE,
+    name_jp varchar(100) UNIQUE,
     website varchar(100),
     geo_lat decimal,
     geo_long decimal,
@@ -12,6 +12,6 @@ CREATE TABLE restaurants (
 DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
     id SERIAL UNIQUE,
-    name varchar(100),
+    name varchar(100) UNIQUE,
     restaurant_count bigint DEFAULT 0
 );
